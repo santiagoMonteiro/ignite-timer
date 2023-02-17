a17b4400b6a1b6d5ac4e5826b4ac278a41291786
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-02-17 18:33:25 -0400

    adding header and default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { History } from './home/History'
 import { Home } from './home/Home'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
