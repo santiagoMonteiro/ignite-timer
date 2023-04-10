b1add6f89d08667128769e9c97e27933146fda90
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-04-10 17:25:49 -0400

    form structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,6 +13,7 @@ import {
   TaskInput,
 } from './styles'
 
+// pattern for validation
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
@@ -21,16 +22,24 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  // form configs
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
-  function handleCreateNewCicle(data: any) {
+  function handleCreateNewCicle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   return (
